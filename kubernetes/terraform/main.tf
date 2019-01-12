9569e8249aab6e84fbc1caf74dce3a6df4971d86
Bob Clarke <bobmclarke@gmail.com>
2019-01-12 22:00:41 +0000

    Adding chart packager

--- kubernetes/terraform/main.tf
+++ kubernetes/terraform/main.tf
@@ -120,7 +120,7 @@ module "redis" {
 }
 
 module "monitoring" {
-  enabled                     = 1
+  enabled                     = 0
   source                      = "modules/monitoring"
   namespace                   = "monitoring"
   domain                      = "${var.domain}"
---
+++ kubernetes/terraform/modules/strapi/package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+helm package chart/strapi -d ~/helm-charts/packaged 
+helm repo index ~/helm-charts/packaged 
+helm repo update 
+#helm search strapi
